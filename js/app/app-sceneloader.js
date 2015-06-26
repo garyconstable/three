94bcc222b2a1f352718b316e5fa9c2052347d390
Gary Constable <gary@yellostudio.co.uk>
2015-06-26 14:05:13 +0100

    added a box geometry

--- js/app/app-sceneloader.js
+++ js/app/app-sceneloader.js
@@ -54,6 +54,45 @@ app.prototype.loadGeometry = function( obj, name, saveData ){
             //console.log('----> Scene load: ' + name)
             
             break;
+            
+            
+        case "BoxGeometry" : 
+            
+            window[name] = new THREE.Mesh(
+            
+                    new THREE.BoxGeometry(
+                        obj.width, 
+                        obj.height, 
+                        obj.depth, 
+                        obj.widthSegments, 
+                        obj.heightSegments, 
+                        obj.depthSegments), 
+                    new THREE.MeshNormalMaterial() 
+            );
+            
+            window[name].overdraw = obj.overdraw;
+            
+            window[name].position.x = obj.position.x;
+            window[name].position.y = obj.position.y;
+            window[name].position.z = obj.position.z;
+            
+            window[name].rotation.x = obj.rotation.x;
+            window[name].rotation.y = obj.rotation.y;
+            window[name].rotation.z = obj.rotation.z;
+            
+            window[name].scale.x = obj.scale.x;
+            window[name].scale.y = obj.scale.y;
+            window[name].scale.z = obj.scale.z;
+            
+            
+            this.sceneObjects[name] = window[name];
+            this.scene.add(window[name]);
+            
+            delete window[name];
+            
+            //console.log('----> Scene load: ' + name)
+            
+            break;
         
     }
     
